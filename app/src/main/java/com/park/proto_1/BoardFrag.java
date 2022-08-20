ce495cf1d2190dfd146e80227e242f8e810267fc
sg Park <qkr1234go@naver.com>
2022-08-20 16:47:42 +0900

    first commit

--- app/src/main/java/com/park/proto_1/BoardFrag.java
+++ app/src/main/java/com/park/proto_1/BoardFrag.java
@@ -2,11 +2,17 @@ package com.park.proto_1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,10 @@ public class BoardFrag extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ArrayList<News> newsArrayList;
+    private String[] newsHeading;
+    private int[] imageResourceID;
+    private RecyclerView recyclerview;
 
     public BoardFrag() {
         // Required empty public constructor
@@ -61,4 +71,47 @@ public class BoardFrag extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_board, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dataInitialize();
+
+        recyclerview = view.findViewById(R.id.recyclerview);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerview.setHasFixedSize(true);
+        MyAdapter myAdapter = new MyAdapter(getContext(),newsArrayList);
+        recyclerview.setAdapter(myAdapter);
+        myAdapter.notifyDataSetChanged();
+    }
+
+    private void dataInitialize() {
+
+        newsArrayList = new ArrayList<>();
+
+        newsHeading = new String[] {
+                getString(R.string.head_1),
+                getString(R.string.head_2),
+                getString(R.string.head_3),
+                getString(R.string.head_4),
+
+        };
+
+        imageResourceID = new int[]{
+                R.drawable.a,
+                R.drawable.b,
+                R.drawable.c,
+                R.drawable.d,
+
+        };
+
+        for(int i=0; i< newsHeading.length; i++) {
+
+            News news = new News(newsHeading[i], imageResourceID[i]);
+            newsArrayList.add(news);
+        }
+
+    }
+
 }
--- app/src/main/java/com/park/proto_1/MainActivity.java
+++ app/src/main/java/com/park/proto_1/MainActivity.java
@@ -2,11 +2,6 @@ package com.park.proto_1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -33,26 +28,26 @@ public class MainActivity extends AppCompatActivity {
         addFrag = new AddFrag();
         proFrag = new ProFrag();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, boardFrag).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, boardFrag).commit();
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
         bottom_menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.first_tab:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, boardFrag).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, boardFrag).commit();
                         return true;
 
                     case R.id.second_tab:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, arcFrag).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, arcFrag).commit();
                         return true;
 
                     case R.id.third_tab:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, addFrag).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, addFrag).commit();
                         return true;
 
                     case R.id.fourth_tab:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, proFrag).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, proFrag).commit();
                         return true;
                 }
                 return false;
